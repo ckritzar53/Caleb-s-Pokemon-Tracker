3731d75c793aecdba8dfcbaa5dcb75b12eea2509
Caleb Kritzar <ckritzar53@busarmydude.org>
2025-08-29 10:46:50 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,41 @@
 // --- DATA ---
-// Data extracted from your CSV files.
-// You can easily add more Pokémon or milestones here.
+// You can easily add more data to these lists.
 
 const DATA = {
     pokedex: [
-        { id: 1, name: "Sprigatito" }, { id: 2, name: "Floragato" }, { id: 3, name: "Meowscarada" },
-        { id: 4, name: "Fuecoco" }, { id: 5, name: "Crocalor" }, { id: 6, name: "Skeledirge" },
-        { id: 7, name: "Quaxly" }, { id: 8, name: "Quaxwell" }, { id: 9, name: "Quaquaval" },
-        { id: 10, name: "Lechonk" }, { id: 11, name: "Oinkologne" }, { id: 12, name: "Tarountula" },
-        { id: 13, name: "Spidops" }, { id: 14, name: "Nymble" }, { id: 15, name: "Lokix" },
-        // This is a small sample. The real tracker would have all 400.
-        // To add more, just continue the pattern: { id: 16, name: "Hoppip" },
+        { id: 1, name: "Sprigatito" }, { id: 2, name: "Floragato" }, { id: 3, "name": "Meowscarada" },
+        { id: 4, name: "Fuecoco" }, { id: 5, name: "Crocalor" }, { id: 6, "name": "Skeledirge" },
+        { id: 7, name: "Quaxly" }, { id: 8, name: "Quaxwell" }, { id: 9, "name": "Quaquaval" },
+        { id: 10, name: "Lechonk" }, { id: 11, name: "Oinkologne" }, { id: 12, "name": "Tarountula" },
+        // This is a sample. To add all 400, continue the pattern.
     ],
-    story: [
-        { name: "The First Day of School" }, { name: "Victory Road: Cortondo Gym" }, { name: "Path of Legends: Stony Cliff Titan" },
-        { name: "Starfall Street: Dark Crew" }, { name: "Victory Road: Artazon Gym" }, { name: "Path of Legends: Open Sky Titan" },
-        { name: "Starfall Street: Fire Crew" }, { name: "Victory Road: Levincia Gym" }, { name: "Path of Legends: Lurking Steel Titan" },
-        { name: "Starfall Street: Poison Crew" }, { name: "Victory Road: Cascarrafa Gym" }, { name: "Path of Legends: Quaking Earth Titan" },
-        { name: "Starfall Street: Fairy Crew" }, { name: "Victory Road: Medali Gym" }, { name: "Path of Legends: False Dragon Titan" },
-        { name: "Starfall Street: Fighting Crew" }, { name: "Victory Road: Montenevera Gym" }, { name: "Victory Road: Alfornada Gym" },
-        { name: "Victory Road: Glaseado Gym" }, { name: "The Way Home" }, { name: "The Championship" },
+    gyms: [
+        { name: "Cortondo Gym", leader: "Katy", type: "Bug", typeColor: "bg-lime-500" },
+        { name: "Artazon Gym", leader: "Brassius", type: "Grass", typeColor: "bg-green-500" },
+        { name: "Levincia Gym", leader: "Iono", type: "Electric", typeColor: "bg-yellow-400" },
+        { name: "Cascarrafa Gym", leader: "Kofu", type: "Water", typeColor: "bg-blue-500" },
+        { name: "Medali Gym", leader: "Larry", type: "Normal", typeColor: "bg-gray-400" },
+        { name: "Montenevera Gym", leader: "Ryme", type: "Ghost", typeColor: "bg-indigo-400" },
+        { name: "Alfornada Gym", leader: "Tulip", type: "Psychic", typeColor: "bg-pink-500" },
+        { name: "Glaseado Gym", leader: "Grusha", type: "Ice", typeColor: "bg-cyan-300" },
+    ],
+    tms: [
+        { id: 1, name: "Take Down" }, { id: 2, name: "Charm" }, { id: 3, name: "Fake Tears" },
+        { id: 4, name: "Agility" }, { id: 5, name: "Mud-Slap" }, { id: 6, name: "Scary Face" },
+        // Sample list of TMs
+    ],
+    items: [
+        { name: "Ability Shield" }, { name: "Amulet Coin" }, { name: "Auspicious Armor" },
+        { name: "Big Root" }, { name: "Black Belt" }, { name: "Black Sludge" },
+        // Sample list of items
     ]
 };
 
 const CATEGORIES = [
-    { id: 'pokedex', title: 'Paldea Pokédex' },
-    { id: 'story', title: 'Story Milestones' }
-    // To add a new category, add an entry here and corresponding data in the DATA object above.
+    { id: 'gyms', title: 'Gyms Completed' },
+    { id: 'tms', title: 'TMs Collected' },
+    { id: 'items', title: 'Items Found' }
 ];
+
+const POKEDEX_STATUSES = ['seen', 'battled', 'caught'];
+
